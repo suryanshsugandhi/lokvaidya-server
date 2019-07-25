a83934fc62b65c07c8fb512b88735f6eca0422d7
Suryansh Sugandhi <31485316+suryanshsugandhi@users.noreply.github.com>
2019-07-25 09:05:54 +0530

    Update and rename server.js to app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const api = require('./routes/api')
 const app = express();
 app.use(cors())
